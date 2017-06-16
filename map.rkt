61d7386763562141d5e89990f258b1c3f749108d
ishan <ishantarunesh@gmail.com>
2017-06-16 15:30:23 +0530

    Scaled Assets, Disabled Mouse

--- map.rkt
+++ map.rkt
@@ -1,36 +1,111 @@
 #lang racket/gui
 (require "sprite.rkt")
 (require 2htdp/image)
-(define game (new frame% [label "Game"] [x 350] [y 50] [height 600] [width 600]))
+
+(define x_sprite 40)
+(define y_sprite 50)
+(define theta 0)
+(define game (new frame% [label "Game"] [x 100] [y 10] [height 700] [width 1200]))
 (define my-canvas%
   (class canvas%
-    (define/override (on-event event)
-      (when (eq? (send event get-event-type) 'motion)
-          (let* ((x (send event get-x))
-                 (y (send event get-y)))
-            ;(print (angle x y))
-            (map))))
+    ;    (define/override (on-event event)
+    ;      (when (eq? (send event get-event-type) 'left-down)
+    ;        (let* ((x (send event get-x))
+    ;               (y (send event get-y)))
+    ;          (set! theta (angle (- x x_sprite) (- y y_sprite)))
+    ;          (send canvas refresh))))
     (define/override (on-char event)
       (let* ((key (send event get-key-code)))
-        (cond ((eq? key 'right) (print "Right"))
-              ((eq? key 'left) (print "Left"))
-              ((eq? key 'up) (print "Up"))
-              ((eq? key 'down) (print "Down")))))
+        (cond ((eq? key 'right) (set! x_sprite (- x_sprite 5)))
+              ((eq? key 'left) (set! x_sprite (+ x_sprite 5)))
+              ((eq? key 'up) (set! y_sprite (+ y_sprite 5)))
+              ((eq? key 'down) (set! y_sprite (- y_sprite 5))))
+        (send canvas refresh)))
     (super-new)))
 (define canvas (new my-canvas% [parent game]
                     [paint-callback (λ (c d)
-                                           (send d draw-bitmap (map) 0 0))]))
+                                      (send d draw-bitmap (image->bitmap (map)) 0 0))]))
 
 (send game show #t)
 
 (define (map)
   (overlay/offset
-   (sprite)
-   40 50
-  (background)))
+   (empty-herbs)
+   -200 -100
+   (overlay/offset
+    (empty-herbs)
+    200 -100
+    (overlay/offset
+     (empty-herbs)
+     -200 100
+     (overlay/offset
+      (empty-herbs)
+      200 100
+      (overlay/offset
+       (empty-herbs)
+       0 0
+       (overlay/offset
+        (empty-herbs)
+        -100 -200
+        (overlay/offset
+         (empty-herbs)
+         100 -200
+         (overlay/offset
+          (empty-herbs)
+          -100 200
+          (overlay/offset
+           (empty-herbs)
+           100 200
+           (overlay/offset
+            (empty-herbs)
+            100 100
+            (overlay/offset
+             (stones)
+             300 -100
+             (overlay/offset
+              (stones)
+              50 200
+              (overlay/offset
+               (stones)
+               400 0
+               (overlay/offset
+                (stones)
+                -300 -200
+                (overlay/offset
+                 (stones)
+                 300 100
+                 (overlay/offset
+                  (stones)
+                  150 -50
+                  (overlay/offset
+                   (stones)
+                   400 -100
+                   (overlay/offset
+                    (stones)
+                    -100 0
+                    (overlay/offset
+                     (stones)
+                     -400 -10
+                     (overlay/offset
+                      (stones)
+                      100 -100
+                      (overlay/offset
+                       (rotate (/ (* -180 theta) pi) (sprite))
+                       x_sprite y_sprite
+                       (background)))))))))))))))))))))))
+   
 (define (background)
-  (rectangle 6000 6000 "solid" (make-color 0 200 0)))
+  (put-pinhole 0 0 (rectangle 1200 700 "solid" (make-color 0 80 60))))
 (define (angle x y)
   (cond ((and (< x 0) (> y 0)) (+ pi (atan (/ y x))))
         ((and (< x 0) (< y 0)) (- (atan (/ y x)) pi))
         (else (atan (/ y x)))))
+
+(define (image->bitmap image)
+  (let* ([width (image-width image)]
+         [height (image-height image)]
+         [bm (make-object bitmap% width height)]
+         [dc (make-object bitmap-dc% bm)])
+    (send dc clear)
+    (send image draw dc 0 0 0 0 width height 0 0 #f)
+    bm))
--- sprite.rkt
+++ sprite.rkt
@@ -5,34 +5,34 @@
 (require lang/posn)
 
 (define (sprite)
-  (underlay/align/offset
-   'center 'bottom
-   (underlay/align/offset
-    'center 'bottom
-    (overlay/align 'center 'center
-                   (rectangle 130 90 "solid" (make-color 204 153 255))
-                   (rectangle 140 100 "solid" (make-color 224 224 224)))
-    0 5
-    (overlay/offset
-     (overlay/align 'center 'center
-                    (circle 20 "solid" (make-color 204 153 255))
-                    (circle 25 "solid" (make-color 224 224 224)))
-     200 0
-     (overlay/align 'center 'center
-                    (circle 20 "solid" (make-color 204 153 255))
-                    (circle 25 "solid" (make-color 224 224 224)))))
-   0 -18
-   (underlay/align/offset
-    'center 'top
-    (overlay/offset
-     (circle 20 "solid" "black")
-     64 0
-     (circle 20 "solid" "black"))
-    -4 6
-    (overlay/offset
-     (circle 7 "solid" "white")
-     64 0
-     (circle 7 "solid" "white")))))
+  (scale 0.3 (underlay/align/offset
+              'center 'bottom
+              (underlay/align/offset
+               'center 'bottom
+               (overlay/align 'center 'center
+                              (rectangle 130 90 "solid" (make-color 204 153 255))
+                              (rectangle 140 100 "solid" (make-color 224 224 224)))
+               0 5
+               (overlay/offset
+                (overlay/align 'center 'center
+                               (circle 20 "solid" (make-color 204 153 255))
+                               (circle 25 "solid" (make-color 224 224 224)))
+                200 0
+                (overlay/align 'center 'center
+                               (circle 20 "solid" (make-color 204 153 255))
+                               (circle 25 "solid" (make-color 224 224 224)))))
+              0 -18
+              (underlay/align/offset
+               'center 'top
+               (overlay/offset
+                (circle 20 "solid" "black")
+                64 0
+                (circle 20 "solid" "black"))
+               -4 6
+               (overlay/offset
+                (circle 7 "solid" "white")
+                64 0
+                (circle 7 "solid" "white"))))))
   
 (define (axe)
   (define wood
@@ -46,56 +46,56 @@
                               (triangle/sss 70 70 10 "solid" (make-color 200 200 200))
                               0 6
                               (triangle/sss 90 90 15 "solid" (make-color 100 100 100))))))
-  (clear-pinhole (overlay/pinhole stone wood)))
+  (scale 0.3 (clear-pinhole (overlay/pinhole stone wood))))
 
 (define (empty-herbs)
-  (add-polygon
-   (add-polygon
-    (add-polygon 
-     (add-polygon
-      (add-polygon
-       (add-polygon
-        (add-polygon 
-         (add-polygon
-          (circle 40 "solid" (make-color 0 153 76))
-          (list (make-posn 72 24)
-                (make-pulled-point 1/6 50 108 34 1/6 -50)
-                (make-posn 70 57))
-          "solid" (make-color 0 153 76))
-         (list (make-posn 55 6)
-               (make-pulled-point 1/6 110 88 -2 1/6 -50)
-               (make-posn 74 27))
-         "solid" (make-color 0 153 76))
-        (list (make-posn 28 10)
-              (make-pulled-point 1/6 80 38 -35 1/6 -80)
-              (make-posn 58 10))
-        "solid" (make-color 0 153 76))
-       (list (make-posn 4 65)
-             (make-pulled-point 1/6 50 -15 30 1/6 -50)
-             (make-posn 30 40))
-       "solid" (make-color 0 153 76))
-      (list (make-posn 20 60)
-            (make-pulled-point 1/4 -50 -10 85 1/4 50)
-            (make-posn 25 90))
-      "solid" (make-color 0 153 76))
-     (list (make-posn 32 86)
-           (make-pulled-point 1/4 -50 20 130 1/4 50)
-           (make-posn 56 112))
-     "solid" (make-color 0 153 76))
-    (list (make-posn 52 111)
-          (make-pulled-point 1/4 -50 74 144 1/4 50)
-          (make-posn 86 107))
-    "solid" (make-color 0 153 76))
-   (list (make-posn 80 108)
-         (make-pulled-point 1/4 -50 122 108 1/4 50)
-         (make-posn 100 80))
-   "solid" (make-color 0 153 76)))
+  (scale 0.3 (add-polygon
+              (add-polygon
+               (add-polygon 
+                (add-polygon
+                 (add-polygon
+                  (add-polygon
+                   (add-polygon 
+                    (add-polygon
+                     (circle 40 "solid" (make-color 0 153 76))
+                     (list (make-posn 72 24)
+                           (make-pulled-point 1/6 50 108 34 1/6 -50)
+                           (make-posn 70 57))
+                     "solid" (make-color 0 153 76))
+                    (list (make-posn 55 6)
+                          (make-pulled-point 1/6 110 88 -2 1/6 -50)
+                          (make-posn 74 27))
+                    "solid" (make-color 0 153 76))
+                   (list (make-posn 28 10)
+                         (make-pulled-point 1/6 80 38 -35 1/6 -80)
+                         (make-posn 58 10))
+                   "solid" (make-color 0 153 76))
+                  (list (make-posn 4 65)
+                        (make-pulled-point 1/6 50 -15 30 1/6 -50)
+                        (make-posn 30 40))
+                  "solid" (make-color 0 153 76))
+                 (list (make-posn 20 60)
+                       (make-pulled-point 1/4 -50 -10 85 1/4 50)
+                       (make-posn 25 90))
+                 "solid" (make-color 0 153 76))
+                (list (make-posn 32 86)
+                      (make-pulled-point 1/4 -50 20 130 1/4 50)
+                      (make-posn 56 112))
+                "solid" (make-color 0 153 76))
+               (list (make-posn 52 111)
+                     (make-pulled-point 1/4 -50 74 144 1/4 50)
+                     (make-posn 86 107))
+               "solid" (make-color 0 153 76))
+              (list (make-posn 80 108)
+                    (make-pulled-point 1/4 -50 122 108 1/4 50)
+                    (make-posn 100 80))
+              "solid" (make-color 0 153 76))))
 ;(define (trees)
 (define (stones)
-  (overlay/align
-   "center" "center"
-   (rotate -10 (pulled-regular-polygon 40 6 1/2 -5 "solid" (make-color 160 160 160)))
-   (rotate 30 (pulled-regular-polygon 50 7 1/2 -5 "solid" (make-color 128 128 128)))))
+  (scale 0.3 (overlay/align
+              "center" "center"
+              (rotate -10 (pulled-regular-polygon 40 6 1/2 -5 "solid" (make-color 160 160 160)))
+              (rotate 30 (pulled-regular-polygon 50 7 1/2 -5 "solid" (make-color 128 128 128))))))
 (define (hammer)
   (define wood
     (put-pinhole 9 9 (overlay/offset
@@ -108,7 +108,7 @@
              "center" "center"
              (rectangle 44 22 "solid" (make-color 200 200 200))
              (rectangle 48 26 "solid" (make-color 100 100 100)))))
-  (clear-pinhole (overlay/pinhole stone wood)))
+  (scale 0.3 (clear-pinhole (overlay/pinhole stone wood))))
 (define (sword)
   (define handle
     (put-pinhole 11 11
@@ -135,7 +135,7 @@
                            (make-posn 70 -2))
                           "solid"
                           (make-color 100 100 100)))))
-  (clear-pinhole (overlay/pinhole blade handle)))
+  (scale 0.3 (clear-pinhole (overlay/pinhole blade handle))))
 
 (define (meat)
   (overlay/align
@@ -143,14 +143,10 @@
    (pulled-regular-polygon 50 3 1/2 -50 "solid" (make-color 255 102 102))
    (pulled-regular-polygon 30 3 1/2 -30 "solid" (make-color 255 255 255))
    ))
-  
-;(meat)
-;(define (fruit-herb)
-;  (overlay/offset
+
 ;(sprite)
 ;(axe)
 ;(hammer)
 ;(sword)
 ;(stones)
 ;(empty-herbs)
-;(fruit-herbs)
